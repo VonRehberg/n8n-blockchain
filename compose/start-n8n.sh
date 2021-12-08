4c0d6c928b666b3187520e356c6f68067763e02e
Joschi Von Rehberg <vonrehberg88@gmail.com>
2021-12-08 12:19:14 +0100

    made node startup correctly and import all workflows - very hacky, but n8n currently has issues with import

--- compose/start-n8n.sh
+++ compose/start-n8n.sh
@@ -3,7 +3,10 @@
 sleep 5
 
 n8n import:credentials --input=/credentials/1.json
-n8n import:workflow --separate --input=/workflows
+ls /workflows/*.json | sed 's/^\([^0-9]*\)\([0-9]*\)/\1 \2/' | sort -k2,2n | tr -d ' ' |
+while read filename; do
+    n8n import:workflow --input=$filename
+done
 
 n8n start
 
